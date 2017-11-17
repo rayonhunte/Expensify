fb9f4460e9972d0a0d8a5a800d7d1b819e50d1f1
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-16 20:15:42 -0400

    clean up

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
 import  validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IndecisionApp from './component/IndecisionApp';
-
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
+// ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
