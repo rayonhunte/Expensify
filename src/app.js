fd45c0edb5a2e16c1cf96f18f3d18b708560bf3f
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-17 03:58:24 -0400

    routes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,50 @@
-import  validator from 'validator';
+import validator from 'validator';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import 'normalize.css/normalize.css'
 import './styles/styles.scss';
 
-// ReactDOM.render(<IndecisionApp/>, document.getElementById('app'))
+const ExpenseDashboard = () => (
+  <div>
+    this is from dashboard component
+  </div>
+);
+
+const CreatePage = () => (
+  <div>
+    this is from add expensify
+  </div>
+);
+
+const EditPage = () => (
+  <div>
+    this is from edit expensify
+  </div>
+);
+
+const HelpPage = () => (
+  <div>
+    this is from help expensify
+  </div>
+);
+
+const NotFoundPage = () => (
+  <div>
+    no such page 404!!
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboard} exact={true}/>
+      <Route path="/create" component={CreatePage}/>
+      <Route path="/edit" component={EditPage}/>
+      <Route path="/help" component={HelpPage}/>
+      <Route component={NotFoundPage}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById('app'))
