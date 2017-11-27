06d09973754ac084c36ada29e6e7e6764fe6d036
Rayon Hunte <rayon.hunte@gmail.com>
2017-11-27 09:04:38 -0400

    delopy bug fix

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
 import numeral from 'numeral';
-import {Link} from 'react-router-dom' ;
 
 
 const ExpensesListItem = ({dispatch, description ,id, amount, createAt})=>(
