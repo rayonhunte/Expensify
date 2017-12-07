f76ce790690b3b9e4a90418e586fa57047d324b0
Rayon Hunte <rayon.hunte@gmail.com>
2017-12-07 16:58:51 -0400

    hidden expenses

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -6,13 +6,15 @@ import selectExpensesTotal from '../selectors/expenses-total';
 import numeral from 'numeral'; 
 
 
-export const ExpensesSummary = ({expensesCount, expensesTotal})=> {
+export const ExpensesSummary = ({expensesCount, expensesTotal, hiddenExpenses})=> {
   const expenseWord = expensesCount === 1 ? 'expense' : 'expenses'; 
   const formatTotal = numeral(expensesTotal / 100).format('$0.0.00'); 
   return (
     <div className="page-header">
       <div className="content-container">
-        <h1 className="page-header__title">Viewing <span>{expensesCount}</span> {expenseWord}, Total <span>{formatTotal}</span></h1>
+        <h2 className="page-header__title">Viewing <span> {expensesCount} </span> 
+            {expenseWord}, Total <span>{formatTotal}</span> Hidden Expenses <span>{hiddenExpenses}</span>
+        </h2>
         <div className="page-header__actions">
           <Link className="button" to="/create"> Add Expense </Link>
         </div>
@@ -26,7 +28,8 @@ const mapStateToProps = (state) => {
   const visibleExpenses = selectExpenses(state.expenses,state.filters);
   return {
     expensesCount: visibleExpenses.length,
-    expensesTotal: selectExpensesTotal(visibleExpenses)
+    expensesTotal: selectExpensesTotal(visibleExpenses),
+    hiddenExpenses: state.expenses.length - visibleExpenses.length
   };
 };
 
